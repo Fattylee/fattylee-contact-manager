b5637c973f91ae59d3ee34ce271b3dfd37aae430
Fatai Balogun <fattylee.remod@gmail.com>
2019-05-24 06:09:04 +0100

    experiment redux in isolation

---
+++ src/playground/redux.js
@@ -0,0 +1,13 @@
+const person = {
+  name: 'Abu Adnaan',
+  email: 'fattylee.remod@gmail.com',
+  address: {
+    ip: '234.738.975.001',
+    city: 'itele lafemwa',
+    state: 'ogun',
+  },
+  stack: ['nodejs', 'javaacript', 'css', 'html5', 'git'],
+};
+
+console.log(person);
+
